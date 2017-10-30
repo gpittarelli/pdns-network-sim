8282519258110d090f71b87257aae7e26f3afd34
gpittarelli <g@gjp.cc>
2017-10-29 22:18:52 -0700

    fix latencies so d2 isn't preferable; various other cleanup+docs

--- topo.py
+++ topo.py
@@ -28,6 +28,11 @@ def char_range(c1, c2):
     for c in xrange(ord(c1), ord(c2)+1):
         yield chr(c)
 
+# Define our simulated root servers. Note that d and d2 should have
+# similar, worse-than-average latencies. The rest of the servers can
+# all be equal, but having 1 or 2 with below average latencies makes
+# the DEWMA behavior (is that still in use?) stand out.
+#
 # Note: These are one-way latencies, so ping times (RTT) will be
 # double the numbers here
 #
@@ -37,11 +42,11 @@ roots = {
     'b':  {'latency': 50,   'ip': '10.0.1.2'},
     'c':  {'latency': 50,   'ip': '10.0.1.3'},
     'd':  {'latency': 100,  'ip': '10.0.1.4'},
-    'd2': {'latency': 10,   'ip': '10.0.2.4'},
+    'd2': {'latency': 100,  'ip': '10.0.2.4'},
     'e':  {'latency': 50,   'ip': '10.0.1.5'},
     'f':  {'latency': 50,   'ip': '10.0.1.6'},
     'g':  {'latency': 50,   'ip': '10.0.1.7'},
-    'h':  {'latency': 50,   'ip': '10.0.1.8'},
+    'h':  {'latency': 30,   'ip': '10.0.1.8'},
     'i':  {'latency': 50,   'ip': '10.0.1.9'},
     'j':  {'latency': 50,   'ip': '10.0.1.10'},
     'k':  {'latency': 50,   'ip': '10.0.1.11'},
@@ -79,13 +84,54 @@ class PdnsTopo(Topo):
 #   sudo mn --custom ~/topo.py --topo pdns --test pingall --link tc
 topos = { 'pdns': ( lambda: PdnsTopo() ) }
 
+
+def runExperiment(net):
+    epoch = time.time()
+    pcapOut = "ipchange-{}.pcap".format(epoch)
+    shutil.copy("/etc/powerdns/root.orig.zone", "/etc/powerdns/root.zone")
+
+    print("Dumping host connections")
+    dumpNodeConnections(net.hosts)
+
+    # debug:
+#    print("Testing network connectivity")
+#    net.pingAllFull()
+
+    client, recursor = net.get('client', 'recursor')
+    rootServers = {
+        letter: net.get('root-{}'.format(letter))
+        for letter, _ in roots.items()
+    }
+
+    print("Launching root servers")
+    serverProcesses = {}
+    for letter, server in rootServers.items():
+        print("Launching root {} on: {}".format(letter, server.IP()))
+
+        serverProcesses[server] = server.popen(
+            "/usr/sbin/pdns_server",
+            "--allow-recursion=no",
+            "--socket-dir=root-{}".format(letter),
+            "--local-address={}".format(server.IP()),
+            "--launch=bind",
+            "--bind-config=/etc/powerdns/bind.conf"
+        )
+
+    print("Launching recursor server")
+    serverProcesses[recursor] = recursor.popen(
+        "/usr/sbin/pdns_recursor",
+        "--local-address={}".format(recursor.IP()),
+        "--hint-file=named.root",
+        "--dont-query="
+    )
+
+
+
 def testPdns():
     epoch = time.time()
     pcapOut = "ipchange-{}.pcap".format(epoch)
-
     shutil.copy("/etc/powerdns/root.orig.zone", "/etc/powerdns/root.zone")
 
-    "Create network and run simple performance test"
     topo = PdnsTopo()
     net = Mininet(
         topo=topo,
@@ -98,11 +144,6 @@ def testPdns():
     print("Dumping host connections")
     dumpNodeConnections(net.hosts)
 
-    # debug:
-#    print("Testing network connectivity")
-#    net.pingAllFull()
-
-    print("Testing bandwidth between h1 and h4")
     client, recursor = net.get('client', 'recursor')
     rootServers = {
         letter: net.get('root-{}'.format(letter))
@@ -134,7 +175,7 @@ def testPdns():
     print("Starting tcpdump")
     tcpdump = recursor.popen("/usr/sbin/tcpdump", "port 53", "-w", pcapOut)
 
-    # Let stuff startup and settle..
+    print("Let stuff startup and settle...")
     time.sleep(2)
 
     print("Starting query traffic")
@@ -143,28 +184,32 @@ def testPdns():
     )
 
     # Length of experiment:
+#    time.sleep(10 * 60)
+    print("Waiting for some baseline traffic...")
     time.sleep(10 * 60)
 
     # d-root IP address switchover
     print("Swapping d-root IP")
     shutil.copy("/etc/powerdns/root.new-d.zone", "/etc/powerdns/root.zone")
     for letter, server in rootServers.items():
-        server.cmdPrint(
+        print("  Swapping over: {}".format(letter))
+        print(server.cmd(
             "pdns_control",
             "--socket-dir=root-{}".format(letter),
             "bind-reload-now",
             "."
-        )
+        ))
 
+    print("Testing new root prime:")
+    print(recursor.cmd(
+        "dig @{} -t ns .".format(rootServers["a"].IP())
+    ))
+
+    print("Waiting for switchover to be picked up...")
     time.sleep(3 * 60 * 60)
 
     print("Stopping query traffic")
     client.cmd('kill %while')
-
-#     for host, line in pmonitor(serverProcesses):
-#         print("hihihi", line)
-#         if host:
-#             print("<%s>: %s" % ( host.name, line ))
 
     print("Killing PowerDNS servers")
     for _, p in serverProcesses.items():
@@ -175,7 +220,7 @@ def testPdns():
 
     net.stop()
 
-    print("Experiment done. Analyze: ")
+    print("Experiment done. Analyze: {}".format(pcapOut))
 
 if __name__ == '__main__':
     testPdns()
